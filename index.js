242a0d3d66ebb212c4b579cdabfd7e8b8e26b8d0
Sichang Li <seeking.scli@gmail.com>
2014-05-10 19:56:40 -0400

    Added code in index.js to read data from index.html

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var app = express();
+var fs = require('fs');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  // response.send('Hello World 2!')
+  response.send(fs.readFileSync('index.html').toString());
 })
 
 app.listen(app.get('port'), function() {
